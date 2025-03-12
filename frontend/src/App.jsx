7d93ac8833452818b8f645da7f89953691d8178b
Popoola Abdulhafeez <hafeezolayinka2005@gmail.com>
2025-03-12 11:08:25 +0100

    add role-based access control; implement RequireAuth component for protected routes and enhance note deletion functionality for managers and admins

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,36 +12,45 @@ import EditNote from './Features/notes/EditNote'
 import NewNote from './Features/notes/NewNote'
 import Prefetch from './Features/auth/Prefetch'
 import PersistLogin from './Features/auth/PersistLogin'
+import RequireAuth from './Features/auth/RequireAuth'
+import { ROLES } from './config/roles'
 
 function App() {
 
     return (
         <Routes>
             <Route path='/' element={<Layout />}>
+                {/* Public Routes */}
                 <Route index element={<Public />} />
                 <Route path='login' element={<Login />} />
 
+                {/* Protected Routes */}
                 <Route element={<PersistLogin />}>
-                    <Route element={<Prefetch />}>
-                        <Route path='dash' element={<DashLayout />}>
+                    <Route element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}>
+                        <Route element={<Prefetch />}>
+                            <Route path='dash' element={<DashLayout />}>
 
-                            <Route index element={<Welcome />} />
+                                <Route index element={<Welcome />} />
 
-                            <Route path='users'>
-                                <Route index element={<UsersList />} />
-                                <Route path=':id' element={<EditUser />} />
-                                <Route path='new' element={<NewUserForm />} />
-                            </Route>
+                                <Route element={<RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />}>
+                                    <Route path='users'>
+                                        <Route index element={<UsersList />} />
+                                        <Route path=':id' element={<EditUser />} />
+                                        <Route path='new' element={<NewUserForm />} />
+                                    </Route>
+                                </Route>
 
-                            <Route path='notes'>
-                                <Route index element={<NotesList />} />
-                                <Route path=':id' element={<EditNote />} />
-                                <Route path='new' element={<NewNote />} />
-                            </Route>
+                                <Route path='notes'>
+                                    <Route index element={<NotesList />} />
+                                    <Route path=':id' element={<EditNote />} />
+                                    <Route path='new' element={<NewNote />} />
+                                </Route>
 
-                        </Route> {/*end Dash */}
+                            </Route> {/*end Dash */}
+                        </Route>
                     </Route>
                 </Route>
+                {/* End Protected Routes */}
 
             </Route>
         </Routes>
