a27024f0b99fedce942a6d028c1754feb7f3f00d
Popoola Abdulhafeez <hafeezolayinka2005@gmail.com>
2025-03-13 12:40:39 +0100

    fix: update allowed origins to include new production URL

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-	'http://localhost:5173',
-	'https://www.dandrepairshop.com',
-	'https://dandrepairshop.com',
+	'https://technotes-vgjc.onrender.com',
 ];
 
 module.exports = allowedOrigins;
