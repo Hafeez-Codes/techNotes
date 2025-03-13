a0fb52244ec1ebd89efdd0711448b18144809561
Popoola Abdulhafeez <hafeezolayinka2005@gmail.com>
2025-03-13 11:44:15 +0100

    feat: add @fvilers/disable-react-devtools for production; update API base URL and disable dev tools in production environment
    Ready to deploy the app to production? let's do it!

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
